a04f17b9583f255a5804daa28716390efe6dca57
Your Name <you@example.com>
2022-03-28 16:07:45 +0200

    test.sh

--- 4_MyStore_Frontend/bin/test.sh
+++ 4_MyStore_Frontend/bin/test.sh
@@ -1,3 +1,3 @@
 env | grep CHROME_BIN
-CHROME_BIN=/usr/bin/google-chrome
-ng test --browsers=ChromeHeadless #--browsers=Headless --no-watch
+CHROME_BIN=/usr/bin/google-chrome karma start --single-run
+ng test --browsers=ChromeHeadless --watch=false
