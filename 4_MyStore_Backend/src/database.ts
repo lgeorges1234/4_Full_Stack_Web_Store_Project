7f16b917744014ce1de73713a15bc1b05b66922c
Your Name <you@example.com>
2022-04-03 17:13:11 +0200

    circleci

--- 4_MyStore_Backend/src/database.ts
+++ 4_MyStore_Backend/src/database.ts
@@ -18,8 +18,8 @@ const {
 // eslint-disable-next-line import/no-mutable-exports
 let client: Pool = new Pool();
 console.log(`ENV var: ${ENV}`);
-console.log(`POSTGRE_DB_TEST: ${POSTGRES_DB_TEST}`)
-console.log(`POSTGRE_DB_DEV: ${POSTGRES_DB_DEV}`)
+console.log(`POSTGRES_DB_TEST: ${POSTGRES_HOST_TEST}`)
+console.log(`POSTGRES_DB_DEV: ${POSTGRES_HOST_DEV}`)
 
 if (ENV === 'test') {
   console.log(`POSTGRE_DB_TEST: ${POSTGRES_DB_TEST}`)
